6e28742fb02f889fbf44a73fb1a0ea1e52d36bc9
danainjax <danainjax@gmail.com>
2021-06-11 10:29:58 -0400

    trying to understand, coding along

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+
+function createEmployeeRecord (array) {
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+// Converts each nested Array into an employee record using createEmployeeRecord and accumulates it to a new Array
+
+function createEmployeeRecords (employeeDataArray) {
+    return employeeDataArray.map(function(array){
+        console.log(createEmployeeRecord(array))
+        return createEmployeeRecord(array)
+    })
+}
+// let createTimeInEvent = function (employee, date){
+//     createEmployeeRecord
+//     return employeeWithDate
+// }
+
+    
+    
